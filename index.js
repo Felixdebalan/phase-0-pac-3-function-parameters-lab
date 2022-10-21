efca02dfda1994b77d6a0fa015b8010a1267f456
Felix De Balanzo <felixdebalan@gmail.com>
2022-10-20 20:55:40 -0400

    commit tree 069f0955e2e7656a2b0e5ea7507e91810931b271

---
+++ index.js
@@ -0,0 +1,10 @@
+
+function introduction (name) {
+    const name = "Aki"
+    
+     console.log(" Hi, my name is ${name}. " )
+}
+function introductionWithLanguage(name, lenguage) {
+    console.group("Hi, my name is ${name} and ${lenguage}.");
+
+}
